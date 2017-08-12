37d6011d91884f47d2601785646bdb302d0636dd
daskinnyman@gmail.com <alex940131@gmail.com>
2017-08-12 20:31:12 +0800

    我也不知道要打甚麼

--- src/screens/lotsPickerPage.js
+++ src/screens/lotsPickerPage.js
@@ -1,9 +1,17 @@
 import React ,{Component} from 'react';
-
+import {Link} from 'react-router';
 class LotsPickerPage extends Component{
   render(){
       return (<div>
                   <h1>抽籤囉</h1>
+                  <p>
+                    哈哈哈!年輕人。留下你的帳號跟生日吧!讓我來決定你今晚要睡哪!
+                  </p>
+                  帳號:<input />
+                  生日:<input />
+                  <Link to="/lr">
+                    <p>確定</p>
+                  </Link>
               </div>);
       }
 }
--- src/screens/lotsResultPage.js
+++ src/screens/lotsResultPage.js
@@ -1,9 +1,12 @@
 import React ,{Component} from 'react';
-
+import {Link} from 'react-router';
 class LotsResultPage extends Component{
   render(){
       return (<div>
                   <h1>邊緣人自己睡</h1>
+                  <Link to="/lP">
+                    <p>重新抽籤</p>
+                  </Link>
               </div>);
       }
 }
