f22105738bd484bc93468d6789bc699826506c79
alex <alex940131@gmail.com>
2017-08-12 18:21:47 +0800

    remove outdated npms and init pages

--- src/container/proto_index.js
+++ src/container/proto_index.js
@@ -5,13 +5,10 @@ import AboutInfos from './about_infos';
 import School from '../components/school.js';
 import Work from './lst_work';
 
-class ProtoIndex extends Component{
+class Home extends Component{
   render(){
       return (<div>
-                  <AboutInfos />
-                  <School />
-                  <Work />
-                  <Footer />
+                  <h1>安安打給後</h1>
               </div>);
       }
 }
---
+++ src/screens/landingPage.js
@@ -0,0 +1,12 @@
+import React ,{Component} from 'react';
+
+class LandingPage extends Component{
+  render(){
+      return (<div>
+                  <h1>安安打給後</h1>
+              </div>);
+      }
+}
+
+
+export default LandingPage;
