2f16ea74e08fedf1cdb25c210ab94392452e591e
alex <alex940131@gmail.com>
2017-08-14 23:08:03 +0800

    add new dependencies

--- src/action/index.js
+++ src/action/index.js
@@ -1,3 +1,6 @@
 import axios from 'axios';
-import csvToJson from 'csvtojson';
 import csvPath from '../../static/testfile.csv';
+
+
+
+
--- src/screens/aboutPage.js
+++ src/screens/aboutPage.js
@@ -1,6 +1,19 @@
 import React ,{Component} from 'react';
+import csv from 'fast-csv';
+import csvPath from '../../static/testfile.csv';
+
+
 
 class AboutPage extends Component{
+ componentWillMount(){
+     console.log(csvPath);
+     csv.fromStream(csvPath)
+     .on("data", (data)=>{
+        console.log(data);
+     });
+ 
+    }
+
   render(){
       return (<div>
                   <h1>關於偉大的開發團隊</h1>
